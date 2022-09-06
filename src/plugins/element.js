8140e40df1dca3ffabd5c84dd91f3de918eb6dac
落叶无情 <909088445@qq.com>
2022-09-06 21:56:13 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,13 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-
+// import { Button } from 'element-ui'
+// import {Form,FormItem} from 'element-ui'
+// import {Input} from 'element-ui'
+// //导入弹框提示组件
+// import {Message} from 'element-ui'
+import {Button,Form,FormItem,Input,Message} from 'element-ui'
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+//把Message挂载到vue的原型对象上，可以通过this.$message获取
+Vue.prototype.$message = Message
