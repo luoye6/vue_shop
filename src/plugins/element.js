08e3bae8ca10d78d7e4e50ae30708073d6b03438
落叶无情 <909088445@qq.com>
2022-09-10 19:31:57 +0800

    完成了权限功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -5,7 +5,9 @@ import Vue from 'vue'
 // //导入弹框提示组件
 // import {Message} from 'element-ui'
 import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem, 
-    Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn,Switch,Tooltip,Pagination,Dialog,MessageBox } from 'element-ui'
+    Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn,Switch,Tooltip,Pagination,Dialog,MessageBox,Tag,Tree,
+    Select,Option
+         } from 'element-ui'
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
@@ -29,6 +31,10 @@ Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Dialog)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
 //把Message挂载到vue的原型对象上，可以通过this.$message获取
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Login from '../components/Login'
 import Home from '../components/Home';
 import Welcome from '../components/Welcome';
 import Users from '../components/user/Users';
+import Rights from '../components/power/Rights';
+import Roles from '../components/power/Roles'
 Vue.use(VueRouter)
 
 const routes = [
@@ -13,7 +15,9 @@ const routes = [
     path: '/home', component: Home, redirect:'/welcome',
     children: [
       { path: '/welcome', component: Welcome },
-      {path:'/users',component:Users}
+      {path:'/users',component:Users},
+      {path:'/rights',component:Rights},
+      {path:'/roles',component:Roles}
     ]
   }
 ]
